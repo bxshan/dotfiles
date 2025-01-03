5d8aea75973459bca27484a3da3be4063841cfc2
Boxuan Shan <boxuan.shan@gmail.com>
2025-01-02 18:41:26 -0800

    +compprog cpp template

---
+++ template.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <utility>
+using namespace std;
+using ll = long long;
+#define fileio(file) freopen(file ".in", "r", stdin); freopen(file ".out", "w", stdout)
+#define FOR(i, n, m) for (int i = (n); i < (m); i++)
+#define ROF(i, n, m) for (int i = (n); i > (m); i--)
+
+int Q;
+
+void solve() {
+
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(nullptr);
+
+  cin >> Q;
+  // uncomment for single queries  
+  //solve()
+  while (Q--) {
+    solve();
+  }
+
+  return 0;
+}
