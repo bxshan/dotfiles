4ee4778df4faac53b8cf006b3777b404c23e1bfe
Boxuan Shan <boxuan.shan@gmail.com>
2025-01-06 10:27:45 -0800

    update

--- template.cc
+++ template.cc
@@ -2,26 +2,19 @@
 #include <utility>
 using namespace std;
 using ll = long long;
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
 #define fileio(file) freopen(file ".in", "r", stdin); freopen(file ".out", "w", stdout)
+#define pout(v); for (auto i : v) {cout << i << " ";} cout << endl;
 #define FOR(i, n, m) for (int i = (n); i < (m); i++)
 #define ROF(i, n, m) for (int i = (n); i > (m); i--)
 
-int Q;
 
-void solve() {
-
-}
-
-int main() {
+signed main() {
   ios::sync_with_stdio(0);
   cin.tie(nullptr);
-
-  cin >> Q;
-  // uncomment for single queries  
-  //solve()
-  while (Q--) {
-    solve();
-  }
 
   return 0;
 }
