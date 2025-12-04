1ffa4b8e3e990e40b66d57ed444252a6eb3bda11
Boxuan Shan <boxuan.shan@gmail.com>
2025-12-04 15:48:40 -0800

    update

--- template.cc
+++ template.cc
@@ -1,20 +1,26 @@
 #include <bits/stdc++.h>
-#include <utility>
 using namespace std;
 using ll = long long;
 #define int long long
 #define pb push_back
 #define mp make_pair
 #define endl '\n'
+#define f first
+#define s second 
+#define INF 0x3f3f3f3f
+#define all(x) (x).begin(), (x).end()
 #define fileio(file) freopen(file ".in", "r", stdin); freopen(file ".out", "w", stdout)
 #define pout(v); for (auto i : v) {cout << i << " ";} cout << endl;
 #define FOR(i, n, m) for (int i = (n); i < (m); i++)
 #define ROF(i, n, m) for (int i = (n); i > (m); i--)
 
+int Q;
 
 signed main() {
   ios::sync_with_stdio(0);
   cin.tie(nullptr);
+
+  cin >> Q;
 
   return 0;
 }
