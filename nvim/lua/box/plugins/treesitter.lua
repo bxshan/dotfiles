17e1dcb9218f85346306ddee648b38b391c50db3
Boxuan Shan <boxuan.shan@gmail.com>
2026-07-19 11:41:10 -0700

    xml

--- nvim/lua/box/plugins/treesitter.lua
+++ nvim/lua/box/plugins/treesitter.lua
@@ -5,6 +5,14 @@ return {
     build = ':TSUpdate',
     config = function()
       require('nvim-treesitter').setup()
+
+      -- Ensure parsers for the languages we actually edit (idempotent: skips
+      -- already-installed ones, so no recompile on startup)
+      require('nvim-treesitter').install({
+        'c', 'cpp', 'java', 'python', 'lua', 'xml',
+        'latex', 'bibtex', 'markdown', 'markdown_inline', 'bash', 'json',
+      })
+
       -- Attach treesitter parser to each buffer so highlighting and foldexpr work
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(ev)
